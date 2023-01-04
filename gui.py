e1ac50f70c09ea5bf3cc261939490215ec695c09
smrrazavian <smrrazavian@outlook.com>
2023-01-05 02:37:31 +0330

    change structure

--- gui.py
+++ gui.py
@@ -1,8 +1,8 @@
 """GUI for the Josephus Problem and visualization of the circular linked list in every step."""
 from tkinter import *
-from structures.linked_list import CircularSinglyLinkedList
-from structures.queue import Queue
-from josephus_problem.logics.josephus import josephus
+# from structures.linked_list import CircularSinglyLinkedList
+# from structures.queue import Queue
+# from josephus_problem.logics.josephus import josephus
 
 
 class JosephusGUI:
@@ -13,6 +13,11 @@ class JosephusGUI:
         self.root.geometry("500x500")
         self.root.resizable(False, False)
         self.root.configure(bg="white")
+        # self.root.iconbitmap("josephus_problem/gui/josephus.ico")
+        # self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        # self.create_widgets()
+        # self.root.mainloop()
+
 
 
 if __name__ == "__main__":
