9786c654a335adb7a1887e3b7c70464aa47c2e8a
smrrazavian <smrrazavian@outlook.com>
2023-01-07 00:45:35 +0330

    creating GUI strucute for project

--- gui.py
+++ gui.py
@@ -1,21 +1,48 @@
 """GUI for the Josephus Problem and visualization of the circular linked list in every step."""
-from tkinter import *
-from structures.linked_list import CircularSinglyLinkedList
-from structures.queue import Queue
+import tkinter as tk
 from josephus import josephus
 
-
-class JosephusGUI:
-    """GUI for the Josephus Problem."""
-    def __init__(self):
-        self.root = Tk()
+class JosephusCanvas():
+    """Canvas Class for the Josephus Problem."""
+    def __init__(self) -> None:
+        self.root = tk.Tk()
         self.root.title("Josephus Problem")
-        self.root.geometry("500x500")
+        self.root.geometry("800x600")
         self.root.resizable(False, False)
-        self.root.configure(bg="white")
-
-
-
+        self.get_inputs().pack()
+        self.canvas = tk.Canvas(self.root, width=800, height=600, bg="white")
+        self.canvas.pack()
+        self.root.mainloop()
+    
+    def get_inputs(self) -> tk.LabelFrame:
+        """Gets the inputs from the user."""
+        frame = tk.LabelFrame(self.root, text="Inputs")
+        frame.grid()
+        n_label = tk.Label(frame, text="Number of people: ")
+        n_label.grid(row=0, column=0)
+        self.n_entry = tk.Entry(frame)
+        self.n_entry.grid(row=0, column=1)
+        k_label = tk.Label(frame, text="Elimination step: ")
+        k_label.grid(row=1, column=0)
+        self.k_entry = tk.Entry(frame)
+        self.k_entry.grid(row=1, column=1)
+        self.submit_button = tk.Button(frame, text="Submit", command=self.submit)
+        self.submit_button.grid(row=2, column=0, columnspan=2)
+        return frame
+    
+    def submit(self) -> None:
+        """Submits the inputs and starts the visualization."""
+        self.n_entry.config(state="disabled")
+        self.k_entry.config(state="disabled")
+        self.submit_button.destroy()
+        self.n = int(self.n_entry.get())
+        self.k = int(self.k_entry.get())
+        self.stages = josephus(self.n, self.k)
+    
+    def draw_circle(self, x: int, y: int, r: int, color: str) -> None:
+        """Draws a circle on the canvas."""
+        self.canvas.create_oval(x-r, y-r, x+r, y+r, fill=color)
+    
 
 if __name__ == "__main__":
-    JosephusGUI()
+    JosephusCanvas()
--- structures/linked_list.py
+++ structures/linked_list.py
@@ -53,3 +53,32 @@ class CircularSinglyLinkedList:
                 print(current.data, end=" ")
                 current = current.next
             print()
+    
+    def __str__(self) -> str:
+        """Returns the string representation of the linked list."""
+        if self.head is None:
+            return "List is empty."
+        else:
+            current = self.head
+            string = str(current.data)
+            current = current.next
+            while current != self.head:
+                string += " " + str(current.data)
+                current = current.next
+            return string
+    
+    def __repr__(self) -> str:
+        """Returns the string representation of the linked list."""
+        return self.__str__()
+    
+    def __len__(self) -> int:
+        """Returns the length of the linked list."""
+        if self.head is None:
+            return 0
+        current = self.head
+        count = 1
+        current = current.next
+        while current != self.head:
+            count += 1
+            current = current.next
+        return count
