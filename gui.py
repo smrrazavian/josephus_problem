aad28a9a117fc6f998ca1e1ff5db17bbc7dd9a2a
smrrazavian <smrrazavian@outlook.com>
2023-01-07 03:01:26 +0330

    debugging canvas size

--- gui.py
+++ gui.py
@@ -5,13 +5,15 @@ from josephus import josephus
 
 class JosephusCanvas():
     """Canvas Class for the Josephus Problem."""
-    def __init__(self) -> None:
+    def __init__(self, width: int, height: int) -> None:
+        self.width = width
+        self.height = height
         self.root = tk.Tk()
         self.root.title("Josephus Problem")
         self.root.geometry("800x600")
         self.root.resizable(False, False)
         self.get_inputs().pack()
-        self.canvas = tk.Canvas(self.root, width=600, height=600, bg="white")
+        self.canvas = tk.Canvas(self.root, width=self.width, height=self.height, bg="white")
         self.canvas.pack()
         self.root.mainloop()
     
@@ -47,28 +49,37 @@ class JosephusCanvas():
         self.canvas.create_text(x, y, text=text, fill="white")
     
     def draw_table(self):
+        """Draws the table on the canvas."""
         while self.stages.size() != 0:
             people = self.stages.dequeue()
             people_count = len(people)
-            r = self.detect_r(people_count)
+            r = self.detect_r(people_count)[0]
             theta = 2 * math.pi / people_count
             for i in range(people_count):
-                x = r * math.cos(i*theta)
-                y = r * math.sin(i*theta)
+                x = self.width/2 + r * math.cos(i*theta)
+                y = self.height/2 + r * math.sin(i*theta)
                 people_list = str(people).split(" ")
                 person = people_list[i]
                 self.draw_people(x, y, r, "blue", person)
+            self.root.update()
+            time = self.detect_r(people_count)[1]
+            self.root.after(time)
+            if self.stages.size() != 0:
+                self.canvas.delete("all")
 
     @staticmethod
-    def detect_r(n:int) -> int:
+    def detect_r(n:int) -> tuple:
+        """Detects the radius of the circle."""
         if n <= 20:
-            r = 50
+            r = 20
+            time = 800
         elif n <= 50:
-            r = 25
+            r = 10
+            time = 1000
         else:
-            r = 15
-        return r
-            
+            r = 5
+            time = 2000
+        return r, time
 
 if __name__ == "__main__":
-    JosephusCanvas()
+    JosephusCanvas(600, 600)
