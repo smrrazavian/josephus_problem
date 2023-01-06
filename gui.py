a03d27b327a868c9effec16f47a2af8fad05150b
mavisbash <aidabashiriann@gmail.com>
2023-01-07 02:34:53 +0330

    drawing table

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 """GUI for the Josephus Problem and visualization of the circular linked list in every step."""
+import math
 import tkinter as tk
 from josephus import josephus
 
@@ -10,7 +11,7 @@ class JosephusCanvas():
         self.root.geometry("800x600")
         self.root.resizable(False, False)
         self.get_inputs().pack()
-        self.canvas = tk.Canvas(self.root, width=800, height=600, bg="white")
+        self.canvas = tk.Canvas(self.root, width=600, height=600, bg="white")
         self.canvas.pack()
         self.root.mainloop()
     
@@ -38,11 +39,36 @@ class JosephusCanvas():
         self.n = int(self.n_entry.get())
         self.k = int(self.k_entry.get())
         self.stages = josephus(self.n, self.k)
+        self.draw_table()
     
-    def draw_circle(self, x: int, y: int, r: int, color: str) -> None:
+    def draw_people(self, x: int, y: int, r: int, color: str, text: str) -> None:
         """Draws a circle on the canvas."""
         self.canvas.create_oval(x-r, y-r, x+r, y+r, fill=color)
+        self.canvas.create_text(x, y, text=text, fill="white")
     
+    def draw_table(self):
+        while self.stages.size() != 0:
+            people = self.stages.dequeue()
+            people_count = len(people)
+            r = self.detect_r(people_count)
+            theta = 2 * math.pi / people_count
+            for i in range(people_count):
+                x = r * math.cos(i*theta)
+                y = r * math.sin(i*theta)
+                people_list = str(people).split(" ")
+                person = people_list[i]
+                self.draw_people(x, y, r, "blue", person)
+
+    @staticmethod
+    def detect_r(n:int) -> int:
+        if n <= 20:
+            r = 50
+        elif n <= 50:
+            r = 25
+        else:
+            r = 15
+        return r
+            
 
 if __name__ == "__main__":
     JosephusCanvas()
