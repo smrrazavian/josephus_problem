d6cf6ccb51de920fa91683ac3912a0119be753fc
smrrazavian <smrrazavian@outlook.com>
2023-01-07 04:26:11 +0330

    think that is the final :) :tada:

--- gui.py
+++ gui.py
@@ -10,7 +10,7 @@ class JosephusCanvas():
         self.height = height
         self.root = tk.Tk()
         self.root.title("Josephus Problem")
-        self.root.geometry("800x600")
+        self.root.geometry(f"{self.width}x{self.height + 200}")
         self.root.resizable(False, False)
         self.get_inputs().pack()
         self.canvas = tk.Canvas(self.root, width=self.width, height=self.height, bg="white")
@@ -31,17 +31,28 @@ class JosephusCanvas():
         self.k_entry.grid(row=1, column=1)
         self.submit_button = tk.Button(frame, text="Submit", command=self.submit)
         self.submit_button.grid(row=2, column=0, columnspan=2)
+        self.reset_button = tk.Button(frame, text="Reset", command=self.reset)
+        self.reset_button.grid(row=3, column=0, columnspan=2)
         return frame
     
     def submit(self) -> None:
         """Submits the inputs and starts the visualization."""
         self.n_entry.config(state="disabled")
         self.k_entry.config(state="disabled")
-        self.submit_button.destroy()
+        self.submit_button.config(state="disabled")
         self.n = int(self.n_entry.get())
         self.k = int(self.k_entry.get())
         self.stages = josephus(self.n, self.k)
         self.draw_table()
+    
+    def reset(self) -> None:
+        """Resets the inputs."""
+        self.n_entry.config(state="normal")
+        self.k_entry.config(state="normal")
+        self.submit_button.config(state="normal")
+        self.canvas.delete("all")
+        self.n_entry.delete(0, "end")
+        self.k_entry.delete(0, "end")
     
     def draw_people(self, x: int, y: int, r: int, color: str, text: str) -> None:
         """Draws a circle on the canvas."""
@@ -54,10 +65,11 @@ class JosephusCanvas():
             people = self.stages.dequeue()
             people_count = len(people)
             r = self.detect_r(people_count)[0]
+            r_2 = self.detect_r(people_count)[2]
             theta = 2 * math.pi / people_count
             for i in range(people_count):
-                x = self.width/2 + r * math.cos(i*theta) - r/2
-                y = self.height/2 + r * math.sin(i*theta) - r/2
+                x = self.width/2 +  r_2 * math.cos(i*theta) - r/2
+                y = self.height/2 + r_2 * math.sin(i*theta) - r/2
                 people_list = str(people).split(" ")
                 person = people_list[i]
                 self.draw_people(x, y, r, "blue", person)
@@ -68,25 +80,22 @@ class JosephusCanvas():
                 self.canvas.delete("all")
 
     @staticmethod
-    def detect_r(n:int) -> tuple:
+    def detect_r(n:int) -> list:
         """Detects the radius of the circle."""
-        # if n <= 20:
-        #     r = 100
-        #     time = 800
-        # elif n <= 50:
-        #     r = 70
-        #     time = 1000
-        # else:
-        #     r = 30
-        #     time = 2000
-        if n >= 50 :
-            r = n*3/4
-        elif n >= 20 :
-            r = n*2
-        else :
-            r = 75 
-        time = 1000
-        return r, time
+        if n <= 20:
+            r = 100
+            r_2 = 300
+            time = 800
+        elif n <= 50:
+            r = 70
+            r_2 = 350
+            time = 1000
+        else:
+            r = 30
+            r_2 = 325
+            time = 2000
+        metrics = [r, time, r_2]
+        return metrics
 
 if __name__ == "__main__":
-    JosephusCanvas(600, 600)
+    JosephusCanvas(800, 800)
